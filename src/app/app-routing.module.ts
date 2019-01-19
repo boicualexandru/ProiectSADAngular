9110ff26c0c6471b36de613ec7489384c6a2daea
Alex Boicu <boicualexandru@mail.com>
2019-01-19 14:34:43 +0200

    Added Graficul-preturilor page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GraficulPreturilorComponent } from './pages/graficul-preturilor/graficul-preturilor.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'graficul-preturilor', component: GraficulPreturilorComponent },
+];
 
 @NgModule({
+  declarations: [
+    HomeComponent,
+    GraficulPreturilorComponent
+  ],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
---
+++ src/app/pages/graficul-preturilor/graficul-preturilor.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-graficul-preturilor',
+    templateUrl: './graficul-preturilor.component.html',
+    styleUrls: ['./graficul-preturilor.component.scss']
+})
+export class GraficulPreturilorComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit(): void { }
+}
